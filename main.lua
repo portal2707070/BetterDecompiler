68d87d1ea96cd9fcad81726399fd637edf5e3e3f
portal2707070 <150728739+portal2707070@users.noreply.github.com>
2024-08-27 15:39:23 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -46,7 +46,7 @@ local originalDecompile = decompile
 -- Sends request to our local port :3
 function BetterDecompiler.cleanScript(uncleanScript, custom_url)
     local HttpService = game:GetService("HttpService")
-    local url = custom_url or "http://localhost:5000/fix_script"
+    local url = custom_url or "http://decompiler.portal2707070.keenetic.pro:5000/fix_script"
     
     local requestBody = {
         script = uncleanScript
