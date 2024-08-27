8e91481af5c758fc0f9835ac6c2ba08536415116
portal2707070 <150728739+portal2707070@users.noreply.github.com>
2024-08-27 18:52:10 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -34,7 +34,7 @@ local RETURN_ELAPSED_TIME = false
 ]]
     loadstring(string.gsub(decompsrc, ";;CONSTANTS HERE;;", CONSTANTS), "Advanced-Decompiler-V3")()
 end
-loaddecomp(100)
+loaddecomp(2)
 -- pls work
 
 
