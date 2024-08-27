ac14ab5774abcb7dff40fbb3047ad4166c1956ec
portal2707070 <150728739+portal2707070@users.noreply.github.com>
2024-08-27 15:41:53 +0200

    Add files via upload

--- flask_ai_server.py
+++ flask_ai_server.py
@@ -45,5 +45,5 @@ def fix_script():
     return jsonify({'fixed_script': formatted_script})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.1.134', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
