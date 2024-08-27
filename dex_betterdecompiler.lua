1582a859d1a44e0b3c5654b02761ee35074e51d5
portal2707070 <150728739+portal2707070@users.noreply.github.com>
2024-08-27 13:49:13 +0200

    Update dex_betterdecompiler.lua

--- dex_betterdecompiler.lua
+++ dex_betterdecompiler.lua
@@ -1,3 +1,25 @@
+-- use celery decompile and after that BetterDecompiler
+local decompsrc = game:HttpGet("https://raw.githubusercontent.com/w-a-e/Advanced-Decompiler-V3/main/init.lua", true)
+local function loaddecomp(decomptimeout)
+    local CONSTANTS = [[
+local ENABLED_REMARKS = {
+    NATIVE_REMARK = false,
+    INLINE_REMARK = false
+}
+local DECOMPILER_TIMEOUT = ]] .. decomptimeout .. [[
+    
+local READER_FLOAT_PRECISION = 99
+local SHOW_INSTRUCTION_LINES = false
+local SHOW_REFERENCES = false
+local SHOW_OPERATION_NAMES = false
+local SHOW_MISC_OPERATIONS = false
+local LIST_USED_GLOBALS = false
+local RETURN_ELAPSED_TIME = false
+]]
+    loadstring(string.gsub(decompsrc, ";;CONSTANTS HERE;;", CONSTANTS), "Advanced-Decompiler-V3")()
+end
+loaddecomp(1)
+-- pls work
 -- https://github.com/LorekeeperZinnia/Dex
 
 --[[
